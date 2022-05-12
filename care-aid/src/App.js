b2484174144a74febc7c6d15cf2be862b03fc31e
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-13 00:34:34 +0600

    Added Navbar & Hero Section

--- care-aid/src/App.js
+++ care-aid/src/App.js
@@ -1,12 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './component/Home/Home';
+import Navbar from './component/Shared/Navbar';
+import About from './component/About/About';
+import Appointment from './component/Appointment/Appointment';
+import Reviews from './component/Reviews/Reviews';
+import Contact from './component/Contact/Contact';
+import Login from './component/Login/Login';
+
 
 function App() {
   return (
     <div >
+       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About></About>} />
+        <Route path="/appointment" element={<Appointment></Appointment>} />
+        <Route path="/reviews" element={<Reviews></Reviews>} />
+        <Route path="/appointment" element={<Appointment></Appointment>} />
+        <Route path="/contact-us" element={<Contact></Contact>} />
+        <Route path="/login" element={<Login></Login>} />
       </Routes>
     </div>
     
--- care-aid/src/component/Home/Home.js
+++ care-aid/src/component/Home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import Navbar from '../Shared/Navbar';
+
 
 const Home = () => {
     return (
-        <div>
-            <Navbar></Navbar>
+        <div className='container mx-auto'>
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src="https://api.lorem.space/image/movie?w=260&h=400" className="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+                        <p className="py-6">Trafalgar provides progressive, and affordable
+                            healthcare, accessible on mobile and online
+                            for everyone</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
