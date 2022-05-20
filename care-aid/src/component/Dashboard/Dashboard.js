fd606d393ac96257f9365a0f5c2a07c11e3274d9
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-21 02:38:29 +0600

    Update Dashboard

--- care-aid/src/component/Dashboard/Dashboard.js
+++ care-aid/src/component/Dashboard/Dashboard.js
@@ -7,11 +7,11 @@ const Dashboard = () => {
             <div className="container mx-auto">
                 <div class="drawer drawer-mobile ">
                     <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-                    <div class="drawer-content  bg-red-100 m-5 lg:p-10 rounded-lg ">
+                    <div class="drawer-content bg-purple-100 m-5 lg:p-10 rounded-lg ">
                         <Outlet />
                         
                     </div>
-                    <div class="drawer-side border border-solid border-slate-200 rounded-lg ">
+                    <div class="drawer-side border border-solid border-slate-200 rounded-lg m-5">
                         <label for="my-drawer-2" class="drawer-overlay"></label>
                         <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
                             {/* <!-- Sidebar content here --> */}
--- care-aid/src/component/MyAppointment/MyAppointment.js
+++ care-aid/src/component/MyAppointment/MyAppointment.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MyAppointment = () => {
     return (
         <div>
-            <h1>Appointment</h1>
+            <h1 className='text-2xl font-bold mb-5'>Appointment</h1>
             <div class="overflow-x-auto">
   <table class="table w-full">
     {/* <!-- head --> */}
